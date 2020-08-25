3e58e4b248a402f7a9b5b0e53046a5437c3e361b
codecowboydotio <svankalken@yahoo.com.au>
2020-08-25 22:21:28 +1000

    updated consumer to be working!

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,4 +1,5 @@
 import twitter
+import requests
 from kafka import KafkaConsumer
 from kafka.errors import KafkaError
 
@@ -8,19 +9,30 @@ consumer_secret=''
 access_token_key=''
 access_token_secret=''
 kafka_topic= 'my-kafka-topic'
-kafka_host='[x.x.x.x:9092]'
+kafka_host='[localhost:9092]'
+up_token = ''
 
 try:
   consumer = KafkaConsumer(kafka_topic, group_id = 'group1',
                            bootstrap_servers = kafka_host)
   for message in consumer:
     print("Topic Name=%s, Message=%s"%(message.topic, message.value))
+    url = message.value.decode('utf-8').strip('\"') # lazy hack 
+    print(url)
+    response = requests.get(url,
+                          headers={"Authorization": up_token}
+                          )
+    data = response.json()
+    value = (data['data']['attributes']['amount']['value'])
+    currency = (data['data']['attributes']['amount']['currencyCode'])
+    print(value, currency)
     api = twitter.Api(consumer_key=consumer_key,
                   consumer_secret=consumer_secret,
                   access_token_key=access_token_key,
                   access_token_secret=access_token_secret)
-    #print(api.VerifyCredentials())
-    #status = api.PostUpdate(message.value)
+    tweet = "Another something just happened in my @up_banking account. This message arrived via #Kafka, and tells me the value of my bank transation was {} {} #api #python #eventdrivenbanking".format(value, currency)
+    print(tweet)
+    status = api.PostUpdate(tweet)
 except Exception as e:
   print(e)
 
